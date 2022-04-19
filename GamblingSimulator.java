bcef9cf8b143d969cb8f0c24af53dc652ca2f59e
firozshaik36 <firozshaik0007@gmail.com>
2022-04-19 17:13:26 +0530

    ADDED:Welcome To Gambling Simulator Program.

---
+++ GamblingSimulator.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.GamblingSimulator;
+
+public class GamblingSimulator {
+
+    /**
+     * As a Gambler Would Start with a Stake
+     * of $100 Every Day and Bet
+     * $1 Every Game .
+     *
+     * Here I am Initializing Static Variables
+     */
+
+    public static final int EveryDayStake = 100;
+    public static final int bet = 1;
+
+    public static void main(String[] args) {
+
+        /**
+         * Added Welcome Message to Understand
+         * what Program it is,
+         */
+
+        System.out.println("---Welcome To Gambling Simulator---");
+
+        }
+    }
+
+
+
